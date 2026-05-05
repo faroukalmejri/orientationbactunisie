daaf3ef43b9cbb88afbc692f01fa325161743ee1
faroukalmejri <farouk.test1@gmail.com>
2026-05-05 10:01:04 +0200

    Add type annotations to request and response serializers
    Updated request and response serializers to satisfy strict TypeScript rules.

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -10,14 +10,16 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      // Added ": any" here to satisfy strict TypeScript rules
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      // Added ": any" here as well
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +27,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
